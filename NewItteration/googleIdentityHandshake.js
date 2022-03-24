12e397f483b18f20c75d02b6e33caaf296e3aa90
Nethiri <marconaumann@t-online.de>
2022-03-24 14:47:57 +0100

    reee

--- NewItteration/googleIdentityHandshake.js
+++ NewItteration/googleIdentityHandshake.js
@@ -1,4 +1,4 @@
-var myclient;
+/* var myclient;
 
 function handleCredentialResponse(response) {
     console.log("Encoded JWT ID token: " + response.credential);
@@ -10,13 +10,13 @@ function handleCredentialResponse(response) {
     '&access_token=' + response.credential + '&tlang=' + 'en');
   xhr.onreadystatechange = function (e) {
   console.log(xhr.response);
-};
-xhr.send(null); 
+  };
+  xhr.send(null); 
 
     /*
     var request =  gapi.client.youtube.captions.download({'id': '9P6rdqiybaw'});
     request.execute(function(response) { console.log(response) });
-    */
+    */ /*
 }
 
 /*window.onload = function () {
@@ -29,7 +29,7 @@ xhr.send(null);
     { theme: "outline", size: "large" }  // customization attributes
   );
   google.accounts.id.prompt(); // also display the One Tap dialog
-}*/
+}*/ /*
 function OldOnloadFunction() {
   google.accounts.id.initialize({
     client_id: "787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com",
@@ -42,30 +42,20 @@ function OldOnloadFunction() {
   google.accounts.id.prompt(); // also display the One Tap dialog
 }
 
-function OldOnloadFunction2() {
-  google.accounts.id.initialize({
-    apiKey: 'AIzaSyDg6gpX1vRkWQ-d4sWFrJMelJEga2zlNrU',
-    clientId: '787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com',
-    scope: 'https://www.googleapis.com/auth/youtube.force-ssl',
-    callback: handleCredentialResponse
-  });
-  google.accounts.id.renderButton(
-    document.getElementById("buttonDiv"),
-    { theme: "outline", size: "large" }  // customization attributes
-  );
-  google.accounts.id.prompt(); // also display the One Tap dialog
-}
 
 
 
-var MyCallback;
-function getToken() {
-  google.accounts.id.initialize({
-    client_id: "787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com",
-    callback: MyCallback
-  });
-
-  google.accounts.id.prompt();
+function newCallbackFunction(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
+  //request following
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET',
+    'https://www.googleapis.com/youtube/v3/captions/9P6rdqiybaw' +
+    '&access_token=' + response.credential + '&tlang=' + 'en');
+  xhr.onreadystatechange = function (e) {
+  console.log(xhr.response);
+  };
+  xhr.send(null); 
 }
 
 
@@ -92,4 +82,26 @@ function trydathing() {
   }
   xhr.addEventListener("error", console.log)
   xhr.send(null);
+}
+
+*/
+
+//=================================
+//=========Promise==================
+//=================================
+
+var GoogleAuth; 
+function initClient() {
+  gapi.client.init({
+    'apiKey': 'AIzaSyDg6gpX1vRkWQ-d4sWFrJMelJEga2zlNrU',
+    'clientId': '787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com',
+    'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
+    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+  }).then(function () {
+    GoogleAuth = gapi.auth2.getAuthInstance();
+
+    // Listen for sign-in state changes.
+    GoogleAuth.isSignedIn.listen(updateSigninStatus);
+});
+
 }
