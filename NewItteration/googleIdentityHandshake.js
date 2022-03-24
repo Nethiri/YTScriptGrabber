85f786fbda487f4a87cbf5c30d30d578cf91ecae
Nethiri <marconaumann@t-online.de>
2022-03-24 14:01:16 +0100

    testerein

--- NewItteration/googleIdentityHandshake.js
+++ NewItteration/googleIdentityHandshake.js
@@ -4,7 +4,7 @@ function handleCredentialResponse(response) {
     console.log("Encoded JWT ID token: " + response.credential);
     //request following
 
-    var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
   xhr.open('GET',
     'https://www.googleapis.com/youtube/v3/captions/9P6rdqiybaw' +
     '&access_token=' + response.credential + '&tlang=' + 'en');
@@ -19,8 +19,19 @@ xhr.send(null);
     */
 }
 
-window.onload = function () {
+/*window.onload = function () {
   myclient = google.accounts.id.initialize({
+    client_id: "787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large" }  // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+}*/
+function OldOnloadFunction() {
+  google.accounts.id.initialize({
     client_id: "787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com",
     callback: handleCredentialResponse
   });
@@ -31,6 +42,7 @@ window.onload = function () {
   google.accounts.id.prompt(); // also display the One Tap dialog
 }
 
+
 var MyCallback;
 function getToken() {
   google.accounts.id.initialize({
@@ -39,4 +51,28 @@ function getToken() {
   });
 
   google.accounts.id.prompt();
+}
+
+
+function start() {
+  gapi.client.init({
+    'apiKey': 'AIzaSyDg6gpX1vRkWQ-d4sWFrJMelJEga2zlNrU',
+    'clientId': '787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com',
+    'scope': 'https://www.googleapis.com/auth/youtube.force-ssl'
+  })
+};
+
+// Load the JavaScript client library and invoke start afterwards.
+gapi.load('client', start);
+
+//gapi.client.getToken().id_token
+
+function trydathing() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET',
+    'https://www.googleapis.com/youtube/v3/captions/9P6rdqiybaw' +
+    '&access_token=' + gapi.client.getToken().id_token + '&tlang=' + 'en');
+  xhr.onreadystatechange = function (e) {
+  console.log(xhr.response);
+  }
 }
